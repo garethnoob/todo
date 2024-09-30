20712d15403d5c13545cf63fb9b8db18fc217ff7
Gareth Davies <gaz.andrew@gmail.com>
2024-09-30 19:59:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [entry, setEntry] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleEntryChange(event) {
+    setEntry(event.target.value)
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    setTodos([...todos, {id:todos.length+1, entry:entry}])
+    setEntry('')
+  }
+
+  function handleDeleteClick(id) {
+    let filteredList = todos.filter(todo => todo.id !== id)
+    setTodos([...filteredList])
+  }
+
+
+
+  return (
+    <>
+      <h1>Todo App</h1>
+      <form className='addEntry' onSubmit={handleFormSubmit}>
+        <input type='text' placeholder='please add a task' value={entry} onChange={handleEntryChange} />
+        <button type='submit'>Submit</button>
+      </form>
+
+      <div className='todoList'>
+        {todos.map(todo => {
+          return (
+              <li key={todo.id}>
+                {todo.entry}
+                <button onClick={()=>handleDeleteClick(todo.id)}>-</button>
+              </li>
+          )
+
+        })}
+      </div>
+    </>
+
+
+  )
+}
+
+export default App
