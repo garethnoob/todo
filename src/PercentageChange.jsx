c587a4951f0401d2d2dfdbd71245817cd06c2fe3
Gareth Davies <gaz.andrew@gmail.com>
2024-10-03 20:56:50 +0100

    Add styling to PercentageChange component and update layout

--- src/PercentageChange.jsx
+++ src/PercentageChange.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './card.css';
 
 
 function PercentageChange() {
@@ -10,12 +11,12 @@ function PercentageChange() {
     const result = (order - forecast) / forecast * 100;
 
     return (
-        <div>
+        <div className="card">
             <h1>Percentage Change</h1>
             <label htmlFor="forecast">Forecast</label>
-            <input type="number" min={0} value={forecast} onChange={(e) => setForecast(e.target.value)} id="forecast"/>
+            <input className="input" type="number" min={0} value={forecast} onChange={(e) => setForecast(e.target.value)} id="forecast"/>
             <label htmlFor="order">Order</label>
-            <input type="number" min={0} value={order} onChange={(e) => setOrder(e.target.value)} id="order"/>
+            <input className="input" type="number" min={0} value={order} onChange={(e) => setOrder(e.target.value)} id="order"/>
             <p>{result? result.toFixed(2):""}%</p>
         </div>
     )
