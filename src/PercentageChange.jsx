8ee58de033a8c2f0a283aac57c63b9d32a310842
Gareth Davies <gaz.andrew@gmail.com>
2024-10-03 18:27:47 +0100

    Update initial state values in PercentageChange component

--- src/PercentageChange.jsx
+++ src/PercentageChange.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 
 function PercentageChange() {
-    const [forecast, setForecast] = useState(0);
-    const [order, setOrder] = useState(0);
+    const [forecast, setForecast] = useState(null);
+    const [order, setOrder] = useState(null);
 
 
 
@@ -13,9 +13,9 @@ function PercentageChange() {
         <div>
             <h1>Percentage Change</h1>
             <label htmlFor="forecast">Forecast</label>
-            <input type="forecast" value={forecast} onChange={(e) => setForecast(e.target.value)} id="forecast"/>
+            <input type="number" value={forecast} onChange={(e) => setForecast(e.target.value)} id="forecast"/>
             <label htmlFor="order">Order</label>
-            <input type="Order" value={order} onChange={(e) => setOrder(e.target.value)} id="order"/>
+            <input type="number" value={order} onChange={(e) => setOrder(e.target.value)} id="order"/>
             <p>{result? result.toFixed(2):""}%</p>
         </div>
     )
